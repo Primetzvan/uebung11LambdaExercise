3987b552ef709948c6920b6279a8f9055b3e6318
primetzvan <vaniprimetz@gmail.com>
2020-01-10 11:17:27 +0100

    Uebung 4

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -16,6 +16,7 @@ public class main {
 
         String fileName = "pupils.csv";
 
+        System.out.println("Filter: \n");
 
             try {
                 Files
@@ -36,6 +37,34 @@ public class main {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+        System.out.println("\nSort:\n");
+
+        try {
+            Files
+                    .readAllLines(Paths.get(fileName), StandardCharsets.UTF_8)
+                    .stream()
+                    .skip(1)
+                    .map(line -> line.split(";"))
+                    .map(lineElement -> new Person( Long.parseLong(lineElement[0]),
+                            lineElement[1],
+                            lineElement[2],
+                            LocalDate.parse(lineElement[3], DateTimeFormatter.ofPattern("dd.MM.yyyy")),
+                            Integer.parseInt(lineElement[4]),
+                            lineElement[5]
+                    ))
+                    .sorted(new Comparator<Person>() {
+                        @Override
+                        public int compare(Person o1, Person o2) {
+                            return o1.getBirthdate().compareTo(o2.getBirthdate());
+                        }
+                    })
+                    .forEach(pupil -> System.out.println(pupil));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         }
 
